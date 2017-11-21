1ba54f449be78980096f589f8731ad1533a9f71a
Tonihuht <toni.huhtinen@metropolia.fi>
2017-11-21 11:12:24 +0200

    FightScreenUpdate

---
+++ Noam's Quest/Assets/FightSystem/Fighters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Fighters
+	{
+		private string name;
+		private int hp;
+		private int attack;
+
+		public Fighters (string name, int hp, int attack)
+		{
+			this.name = name;
+			this.hp = hp;
+			this.attack = attack;
+		}
+	}
+}
+
