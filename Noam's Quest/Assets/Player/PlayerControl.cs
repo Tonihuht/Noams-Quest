4a7a8f35d2d0be144ed5c89806993394676f135c
MrJoXuX <joackimd@metropolia.fi>
2017-11-21 12:20:30 +0200

    TutorialUpdate1
    Player movement prototype and tutorials update

---
+++ Noam's Quest/Assets/Player/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public int playerSpeed = 10;
+	private float moveX;
+	private float moveY;
+
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerMoves ();
+	}
+
+	public void PlayerMoves () {
+		//Controls for the player
+		moveX = Input.GetAxis ("Horizontal");
+		moveY = Input.GetAxis ("Vertical");
+
+		//Physics for the player
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveX * playerSpeed, moveY * playerSpeed);
+	}
+}
