5462b41e6a0208ecd8a12cf6de33107b1141bc22
MrJoXuX <joackimd@metropolia.fi>
2017-11-24 12:35:45 +0200

    InventoryStart
    The start of the inventory system

--- Noam's Quest/Assets/Scripts/Inventory Scripts/Item.cs
+++ Noam's Quest/Assets/Scripts/Inventory Scripts/Item.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour {
+public enum ItemType {HEALTH, KEY};
 
+public class Item : MonoBehaviour {
+
+	public ItemType type;
+	public Sprite spriteNeutral;
+	public Sprite spriteHighlighted;
 	// Use this for initialization
 	void Start () {
 		
