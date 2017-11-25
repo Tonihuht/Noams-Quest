1227ce2c045f0b88afdd5c454360a2f98a5cde4e
Tonihuht <toni.huhtinen@metropolia.fi>
2017-11-25 03:59:29 +0200

    TutorialFight3

---
+++ Noam's Quest/Assets/Scripts/BattleMenuNavigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleMenuNavigation : MonoBehaviour {
+
+
+	public void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Debug.Log ("Attack");
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Debug.Log ("Block");
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Debug.Log ("Dodge");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Debug.Log ("Inventory");
+		}
+	}
+}
--- Noam's Quest/Assets/Scripts/CombatSystem.cs
+++ Noam's Quest/Assets/Scripts/CombatSystem.cs
@@ -1,9 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CombatSystem : MonoBehaviour {
 
+	int enemyHp = 100;
+	int playerHp = 100;
+	int enemyDmg = 10;
+	int playerDmg = 20;
+	int playerIndex = 0;
+	int enemyIndex = 0;
 
+
+
+
+	public void Turns () {
+
+		if (playerHp <= 0 || enemyHp <= 0)
+		while (playerIndex <= enemyIndex) {
+			
+			enemyHp = enemyHp - playerDmg;
+
+			playerIndex++;
+		}
+		playerHp = playerHp - enemyDmg;
+
+		enemyIndex++;
+
+	}
 
 }
