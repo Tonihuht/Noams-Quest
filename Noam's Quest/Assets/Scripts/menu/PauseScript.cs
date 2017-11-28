f44531aca6ef19e373b9403299b6d9063148a97d
benjamga <33685848+benjamga@users.noreply.github.com>
2017-11-28 11:06:08 +0200

    mainmenu+pausemenu
    add menu

---
+++ Noam's Quest/Assets/Scripts/menu/PauseScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour {
+
+    GameObject Pausemenu;
+    bool paused;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+        Pausemenu = GameObject.Find("Pausemenu");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+        if (paused)
+        {
+            Pausemenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            Pausemenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+		
+	}
+    public void ResumeButton()
+    {
+        paused = false;
+    }
+    public void MainMenuB()
+    {
+        Application.LoadLevel(1);
+    }
+    public void SaveButton()
+    {
+        PlayerPrefs.SetInt("currentscenesave", Application.loadedLevel);
+    }
+    public void LoadButton()
+    {
+        Application.LoadLevel(PlayerPrefs.GetInt("currentscenesave"));
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
